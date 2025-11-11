1098754932a8c0257c9492cfd1e3bfae38a2b3c0
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-11-11 23:28:00 +0530

    soln 1

---
+++ 2_conditionals/1_solution.py
@@ -0,0 +1,15 @@
+age = 65
+
+
+if age < 13:
+    print("Child")
+elif age < 20:
+    print("Teenager")
+elif age < 60:
+    print("Adult")
+else:
+    print("Senior")
+
+# OP: 25 -> Adult
+# OP: 20 -> Adult
+# OP: 65 -> Senior
